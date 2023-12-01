48cb5c05149fc12622077c984580d839b77d25f5
Rinasketch <148871412+Rinasketch@users.noreply.github.com>
2023-12-01 11:35:58 +0900

    Add files via upload

---
+++ calculator3/calculator3/main.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class AddOperation {
+    func operate(num1: Double, num2: Double) -> Double {
+        return num1 + num2
+    }
+}
+        
+class SubstractOperation {
+    func operate(num1: Double, num2: Double) -> Double {
+        return num1 - num2
+    }
+}
+        
+class MultiplyOperation {
+    func operate(num1: Double, num2: Double) -> Double {
+        return num1 * num2
+    }
+}
+        
+class DivideOperation {
+    func operate(num1: Double, num2: Double) -> Double {
+        return num1 / num2
+    }
+}
+
+class Calcurator {
+
+
+}
+//미완성...
+
